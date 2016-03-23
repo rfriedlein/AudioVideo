d81c55a5a23529a6ae50d115e30e55dc7b1096f5
Rob Frieldein <robyn@rfriedlein.com>
2016-03-23 14:09:22 -0400

    Create install_asterisk13_vanilla.sh

---
+++ asterisk13/install_asterisk13_vanilla.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+##################################
+## rfrieldein.com 03/23/2016    ##
+## robyn@rfriedlein.com         ##
+## v0.16                        ##
+## Install Asterisk 13 Latest   ##
+##################################
+
+export ASTERISK_DB_PW=`dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 - | cut -c2-18`
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+
+#Install some stuff
+apt-get update && apt-get upgrade -y
+apt-get install build-essential wget curl git libssl-dev libncurses5-dev libreadline-dev libreadline6-dev libnewt-dev libxml2-dev linux-headers-$(uname -r) libsqlite3-dev libiksemel-dev libmyodbc libsqlite3-dev libjansson-dev uuid-dev libxslt1-dev liburiparser-dev pkg-config subversion libspandsp-dev libiksemel-utils libiksemel3 libasound2-dev libogg-dev libvorbis-dev libcurl4-openssl-dev libical-dev libneon27-dev libsrtp0-dev automake libtool autoconf unixodbc-dev uuid mpg123 sqlite3 bison flex php5 php5-curl php5-cli php5-mysql php-pear php-db php5-gd curl sox openssh-server apache2 dahdi -y
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get install mysql-server mysql-client mysql-common libmysqlclient-dev -y
+mysql-conn_test
+
+#Install pjproject
+cd /usr/src/
+wget http://www.pjsip.org/release/2.4/pjproject-2.4.tar.bz2
+tar -xjvf pjproject-2.4.tar.bz2
+cd pjproject-2.4
+ ./configure --prefix=/usr --enable-shared --disable-sound --disable-resample --disable-video --disable-opencore-amr CFLAGS='-O2 -DNDEBUG'
+make dep && make && make install 
+ldconfig
+
+#Install libpri
+cd /usr/src/
+wget http://downloads.asterisk.org/pub/telephony/libpri/libpri-1.4-current.tar.gz
+tar zxvf libpri*
+cd /usr/src/libpri*
+make && make install
+
+#Install jansson 
+cd /usr/src/
+git clone https://github.com/akheron/jansson.git
+cd /usr/src/jansson
+autoreconf -i
+./configure
+make
+make install
+
+#Asterisk Extra Sounds
+mkdir -p /var/lib/asterisk/sounds
+cd /var/lib/asterisk/sounds
+wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-extra-sounds-en-wav-current.tar.gz
+tar xfz asterisk-extra-sounds-en-wav-current.tar.gz
+rm -f asterisk-extra-sounds-en-wav-current.tar.gz
+# Wideband Audio download
+wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-extra-sounds-en-g722-current.tar.gz
+tar xfz asterisk-extra-sounds-en-g722-current.tar.gz
+rm -f asterisk-extra-sounds-en-g722-current.tar.gz
+
+#build asterisk
+cd /usr/src/
+wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-13-current.tar.gz
+tar zxvf asterisk*
+cd /usr/src/asterisk*
+./contrib/scripts/get_mp3_source.sh
+./configure && make menuselect && make && make install && make config && make samples
+
+useradd -m asterisk
+chown asterisk. /var/run/asterisk
+chown -R asterisk. /etc/asterisk
+chown -R asterisk. /var/{lib,log,spool}/asterisk
+chown -R asterisk. /usr/lib/asterisk
+
+sed -i 's/\(^upload_max_filesize = \).*/\120M/' /etc/php5/apache2/php.ini
+cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf_orig
+sed -i 's/^\(User\|Group\).*/\1 asterisk/' /etc/apache2/apache2.conf
+service apache2 restart
+
+#MySQL DB's
+
+mysqladmin -u root create asterisk
+mysqladmin -u root create asteriskcdrdb
+
+mysql -u root -e "GRANT ALL PRIVILEGES ON asterisk.* TO asteriskuser@localhost IDENTIFIED BY '${ASTERISK_DB_PW}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON asteriskcdrdb.* TO asteriskuser@localhost IDENTIFIED BY '${ASTERISK_DB_PW}';"
+mysql -u root -e "flush privileges;"
+
+echo "When asked for database credentials, use these."
+echo "Database username: asteriskuser"
+echo "Database password: $ASTERISK_DB_PW"
+echo "Be sure to save these!"
+
+/etc/init.d/asterisk stop
+
+./start_asterisk start
+
+ln -s /var/lib/asterisk/moh /var/lib/asterisk/mohmp3
+
+service asterisk stop
+service asterisk start
